bbcfa9b3e8af48132bc3b44fe16b88b7f3b55be5
tamermoghraby <tamermg8@gmail.com>
2024-02-16 18:54:44 +0200

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Camp from "@/components/Camp";
+import Developer from "@/components/Developer";
 import Features from "@/components/Features";
 import GetApp from "@/components/GetApp";
 import Guide from "@/components/Guide";
@@ -7,11 +8,12 @@ import Hero from "@/components/Hero";
 export default function Home() {
   return (
     <>
+      <Developer />
       <Hero />
       <Camp />
       <Guide />
       <Features />
       <GetApp />
     </>
-  )
+  );
 }
