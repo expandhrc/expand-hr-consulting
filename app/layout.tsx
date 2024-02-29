9268e111f78c543bc81b1cae8392c789fc32738f
Tamer Al Moghraby <106259595+tamermoghraby@users.noreply.github.com>
2024-02-29 05:41:03 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html>
       <body>
         <Navbar />
-        <main className="relative bg-white overflow-hidden">{children}</main>
+        <main className="relative overflow-hidden">{children}</main>
         <Footer />
       </body>
     </html>
