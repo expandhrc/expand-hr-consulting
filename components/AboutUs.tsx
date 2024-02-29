e4f96271877161a87e14c5dd5dd342dfb080b6b9
Tamer Al Moghraby <106259595+tamermoghraby@users.noreply.github.com>
2024-02-29 04:33:54 +0200

    Adjudt about page text size

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -4,12 +4,12 @@ import Button from "./Button";
 const AboutUs = () => {
   return (
     <section
-      className="max-container bg-[#f0f8ff] rounded-xl padding-container 
+      className="max-container rounded-xl padding-container 
     flex flex-col gap-6 xl:gap-20 py-10 mb-4"
     >
       <div className="relative z-20 flex flex-1 flex-col ">
         <h2 className="bold-40 lg:bold-52 text-indigo-900">About Us</h2>
-        <p className="regular-20 mt-6 text-gray-50 ">
+        <p className="regular-16 lg:regular-20 mt-6 text-gray-50 ">
           In establishing our consultancy firm, we envisioned a dynamic space
           where expertise and innovation converge to catalyze positive change.
           Founded in 2009, our journey is fueled by a passion for empowering
